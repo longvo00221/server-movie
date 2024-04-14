057066da0b2eec6c0cfd59175c9e3c71577cbeeb
longvo00221 <longvo010203@gmail.com>
2024-04-14 21:06:35 +0700

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,9 @@ import userRoute from "./user.route.js";
 import mediaRoute from "./media.route.js";
 import personRoute from "./person.route.js";
 import reviewRoute from "./review.route.js";
-import cors from "cors";
 
 const router = express.Router();
-router.use(cors({ origin: "*" }));
+
 router.use("/user", userRoute);
 router.use("/person", personRoute);
 router.use("/reviews", reviewRoute);
